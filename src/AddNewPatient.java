9609952db8d1a4ac8e7b967385955bfbbe12b876
MustafaS1996 <syed.sarwar@sjsu.edu>
2019-11-27 15:38:27 -0800

    Connecting DB

--- src/AddNewPatient.java
+++ src/AddNewPatient.java
@@ -28,13 +28,15 @@ public class AddNewPatient extends JFrame {
 
 	
 	
-	public static String USERNAME = new String("keon");
-	public static String PASSWORD = new String("7eu6Y.La=VJh");
+	public static String USERNAME = new String("root");
+	public static String PASSWORD = new String("Roosted2!");
 
 	//	public static String DB_URL = new String("jdbc:mysql://localhost/test?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
-	public static String DB_URL = new String("jdbc:mysql://localhost:3306/project");
+	public static String DB_URL = new String("jdbc:mysql://localhost:3306/test");
 	public static String JDBC_DRIVER = new String("com.mysql.cj.jdbc.Driver");
 	private JPanel contentPane;
+	private int THC = 0;
+	private String CDate = "11-27-19";
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
@@ -159,7 +161,6 @@ public class AddNewPatient extends JFrame {
 				  if (flagCheckString(textField.getText()) == true && 
 				  	  flagCheckString(textField_1.getText()) == true &&
 					  flagCheckString(textField_2.getText()) == true &&
-					  flagCheckInt(textField_3.getText()) == true &&
 					  flagCheckString(textField_4.getText()) == true &&
 					  flagCheckInt(textField_5.getText()) == true && //TextField 6 & 7 can be mix of digits and strings
 					  flagCheckString(textField_8.getText()) == true &&
@@ -188,11 +189,86 @@ public class AddNewPatient extends JFrame {
 				       SSN = textField_13.getText();
 				       insurance = textField_14.getText();
 
-				       //
+				       
 				       if (!fName.equals("") && !lName.equals("") && !dOB.equals("") &&
 				    		   !gender.equals("") && !phone.equals("") && !street.equals("") 
 				    		   && !city.equals("") && !zipCode.equals("") && !country.equals("") && !SSN.equals(""))
 				       {
+				    	   
+				
+							
+							
+							THC = THC+1;
+							String[] InsertRows = {"INSERT INTO Patient(THC, CurrentDate, First_name, Middle_name, Last_name, Date_of_Birth,\n" + 
+									"				Gender, Phone, Email, Street_Address, City, State_ID, ZIP_ID, Country_ID , Photo,\n" + 
+									"				Social_Security_Number, Insurnace)\n" + 
+									"				VALUES("+THC+",'"+CDate+"','"+fName+"','"+mName+"','"+lName+"','"+dOB+"','"+gender+"',"+Integer.parseInt(phone)+",'"+email+"','"+street+"','"+city+"','"+state+"',"+Integer.parseInt(zipCode)+",'"+country+"','"+photo+"',"+SSN+",'"+insurance+"',(SELECT STATE_ID FROM REF_State WHERE Name = 'CALIFORNIA'), \n" + 
+									"                (SELECT ZIP_ID FROM REF_Zip WHERE Name = "+Integer.parseInt(zipCode)+"),\n" + 
+									"				(SELECT Country_ID FROM REF_Country WHERE Name = '"+country+"'),'"+photo+"','"+SSN+"','"+insurance+"');"};
+							
+							
+							for (int a=0; a<InsertRows.length; ++a)
+							{
+								System.out.println(InsertRows[a]);
+							}
+
+
+							try {
+								Class.forName(JDBC_DRIVER);
+								try
+								{
+								          // Get a connection from the connection factory
+									Connection con = DriverManager.getConnection(
+									DB_URL,
+									  //"jdbc:oracle:thin:@dbaprod1:1521:SHR1_PRD",
+									USERNAME, PASSWORD);
+							
+									// Show some database/driver metadata
+									SQLUtil.printDriverInfo(con);
+
+									// Create a Statement object so we can submit SQL statements to the driver
+									Statement stmt = con.createStatement();
+
+									// Submit the statement
+									for (int i=0; i<InsertRows.length; ++i)
+									{
+										System.out.print(InsertRows[i] + "...");
+										int rowsAffected = stmt.executeUpdate(InsertRows[i]);
+										if (rowsAffected == 1)
+											System.out.println("OK");
+									}
+
+									// Close the statement
+									stmt.close();
+
+									// Close the connection
+									con.close();
+								}
+								catch (SQLException e1)
+								{
+						          	 	SQLUtil.printSQLExceptions(e1);		
+						         }
+							
+							} //try for Class.forName(JDBC_DRIVER);
+							catch (ClassNotFoundException e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}//catch		
+							
+				    	   
+				    	   
+				    	   
+				    	   
+				    	   
+				    	   
+				    	   
+				    	   
+				    	   
+				    	   
+				    	   
+				    	   
+				    	   
+				    	   
 				    	   
 				       	p.addPatient(fName,mName,lName,dOB,gender, 
 					    phone,email,street,city,state,zipCode,country,photo,SSN,insurance);
@@ -448,7 +524,7 @@ public class AddNewPatient extends JFrame {
 
 				
 	}
-	public static void main(String args[]) throws SQLException {
+	//public static void main(String args[]) throws SQLException {
 		
 		///* Bring patient */		
 				// Connect to the database
@@ -518,70 +594,66 @@ public class AddNewPatient extends JFrame {
 				insert into Patient(THC, Country_ID,State_ID,ZIP_ID,WStatus_ID,Occup_ID,Surname,First_name) values ('c', 0,0,0,0,0,'name','Endalk');
 
 				 */
-				String newInput = "";
-				String[] InsertRows = {"INSERT INTO Patient(THC, CurrentDate, First_name, Middle_name, Last_name, Date_of_Birth,\n" + 
-						"Gender, Phone, Email, Street_Address, City, State_ID, ZIP_ID, Country_ID , Photo,\n" + 
-						"Social_Security_Number, Insurnace)\n" + 
-						"VALUES(a,a,a,a,a,a,a,a,123,a,(SELECT STATE_ID FROM REF_State WHERE Name = ''), (SELECT ZIP_ID FROM REF_Zip WHERE Name = ''),\n" + 
-						"(SELECT Country_ID FROM REF_Country WHERE Name = ''),a,a,a);"};
+//				String newInput = "";
+//			
+//				
+//				String[] InsertRows = {"INSERT INTO Patient(THC, CurrentDate, First_name, Middle_name, Last_name, Date_of_Birth,\n" + 
+//						"				Gender, Phone, Email, Street_Address, City, State_ID, ZIP_ID, Country_ID , Photo,\n" + 
+//						"				Social_Security_Number, Insurnace)\n" + 
+//						"				VALUES('abc','2019-11-01','Keon','middle','Min','1993-03-12','F',415999999,'email','street','city',(SELECT STATE_ID FROM REF_State WHERE Name = 'CALIFORNIA'), \n" + 
+//						"                (SELECT ZIP_ID FROM REF_Zip WHERE Name = 95112),\n" + 
+//						"				(SELECT Country_ID FROM REF_Country WHERE Name = 'ETHIOPHIA'),'Photo','SSN','Insurance');"};
+//				
+//				
+//				for (int a=0; a<InsertRows.length; ++a)
+//				{
+//					System.out.println(InsertRows[a]);
+//				}
+//
+//
+//				try {
+//					Class.forName(JDBC_DRIVER);
+//					try
+//					{
+//					          // Get a connection from the connection factory
+//						Connection con = DriverManager.getConnection(
+//						DB_URL,
+//						  //"jdbc:oracle:thin:@dbaprod1:1521:SHR1_PRD",
+//						USERNAME, PASSWORD);
+//				
+//						// Show some database/driver metadata
+//						SQLUtil.printDriverInfo(con);
+//
+//						// Create a Statement object so we can submit SQL statements to the driver
+//						Statement stmt = con.createStatement();
+//
+//						// Submit the statement
+//						for (int i=0; i<InsertRows.length; ++i)
+//						{
+//							System.out.print(InsertRows[i] + "...");
+//							int rowsAffected = stmt.executeUpdate(InsertRows[i]);
+//							if (rowsAffected == 1)
+//								System.out.println("OK");
+//						}
+//
+//						// Close the statement
+//						stmt.close();
+//
+//						// Close the connection
+//						con.close();
+//					}
+//					catch (SQLException e1)
+//					{
+//			          	 	SQLUtil.printSQLExceptions(e1);		
+//			         }
+//					////
+//					
+//				} //try for Class.forName(JDBC_DRIVER);
+//				catch (ClassNotFoundException e1) {
+//					// TODO Auto-generated catch block
+//					e1.printStackTrace();
+//				}//catch		
+//				
 				
-				String[] newInsertRows = {"INSERT INTO Patient(THC, CurrentDate, First_name, Middle_name, Last_name, Date_of_Birth,\n" + 
-						"				Gender, Phone, Email, Street_Address, City, State_ID, ZIP_ID, Country_ID , Photo,\n" + 
-						"				Social_Security_Number, Insurnace)\n" + 
-						"				VALUES('abc','2019-11-01','Keon','middle','Min','1993-03-12','F',415999999,'email','street','city',(SELECT STATE_ID FROM REF_State WHERE Name = 'CALIFORNIA'), \n" + 
-						"                (SELECT ZIP_ID FROM REF_Zip WHERE Name = 95112),\n" + 
-						"				(SELECT Country_ID FROM REF_Country WHERE Name = 'ETHIOPHIA'),'Photo','SSN','Insurance');"};
-				
-				
-				for (int a=0; a<InsertRows.length; ++a)
-				{
-					System.out.println(InsertRows[a]);
-				}
-
-
-				try {
-					Class.forName(JDBC_DRIVER);
-					try
-					{
-					          // Get a connection from the connection factory
-						Connection con = DriverManager.getConnection(
-						DB_URL,
-						  //"jdbc:oracle:thin:@dbaprod1:1521:SHR1_PRD",
-						USERNAME, PASSWORD);
-				
-						// Show some database/driver metadata
-						SQLUtil.printDriverInfo(con);
-
-						// Create a Statement object so we can submit SQL statements to the driver
-						Statement stmt = con.createStatement();
-
-						// Submit the statement
-						for (int i=0; i<InsertRows.length; ++i)
-						{
-							System.out.print(InsertRows[i] + "...");
-							int rowsAffected = stmt.executeUpdate(InsertRows[i]);
-							if (rowsAffected == 1)
-								System.out.println("OK");
-						}
-
-						// Close the statement
-						stmt.close();
-
-						// Close the connection
-						con.close();
-					}
-					catch (SQLException e1)
-					{
-			          	 	SQLUtil.printSQLExceptions(e1);		
-			         }
-					////
-					
-				} //try for Class.forName(JDBC_DRIVER);
-				catch (ClassNotFoundException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}//catch		
-				
-				
-	}
+	//}
 }
