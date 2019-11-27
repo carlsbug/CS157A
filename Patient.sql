b2131bb317118c56a2823d632852c65b38b164d3
carlsbug <keonwoong.min@sjsu.edu>
2019-11-27 14:35:02 -0800

    updated

--- Patient.sql
+++ Patient.sql
@@ -31,7 +31,7 @@ CREATE TABLE REF_Zip(
 CREATE TABLE Patient(
 	THC varchar(6) not null,
 	CurrentDate DATE,
-	First_name VARCHAR(15),
+	First_name VARCHAR(15) not null,
 	Middle_name VARCHAR(15),
 	Last_name VARCHAR(15) not null,
 	Date_of_Birth DATE not null,
@@ -40,14 +40,29 @@ CREATE TABLE Patient(
 	Email VARCHAR(50),
 	Street_Address VARCHAR(60) not null,
 	City VARCHAR(30) not null,
-	State_ID SMALLINT not null,
+	State_ID SMALLINT,
 	ZIP_ID SMALLINT not null,
 	Country_ID SMALLINT not null,
 	Photo VARCHAR(1000),
 	Social_Security_Number VARCHAR(10),
 	Insurnace VARCHAR(10),
+
+  -- Demographic
 	WStatus_ID SMALLINT not null,
 	Occup_ID SMALLINT not null,
+	Educational_Degree VARCHAR(10),
+	Tin_background CHAR(1),
+	H_background CHAR(1),
+	Tin_When Date,
+	H_When Date
+	-- T_lnd_comments VARCHAR(10000),
+	-- H_lnd_comments VARCHAR(10000)
+	-- 
+	-- Tinnitus_Onset VARCHAR(10),
+	-- Tinnitus_Etiology VARCHAR(10),
+	-- Hyperacusis_Onset VARCHAR(10),
+	-- Hyperacusis_Etiology VARCHAR(10),
+
 
 	CONSTRAINT PK_PATIENT PRIMARY KEY (THC),
 	CONSTRAINT FK_PATIENT_REFERENCE_REF_COUN FOREIGN KEY(Country_ID) REFERENCES REF_Country(Country_ID) ON UPDATE CASCADE
@@ -69,7 +84,7 @@ CREATE TABLE Visit(
 	THC VARCHAR(1) not null,
 	Visit_nr SMALLINT not null,
 	Date DATETIME not null,
-	Comments VARCHAR(10000),
+	Comments VARCHAR(150),
 
 	CONSTRAINT PK_VISIT PRIMARY KEY (Visit_ID),
 	CONSTRAINT FK_VISIT_REFERENCE_PATIENT FOREIGN KEY(THC) REFERENCES Patient(THC) ON UPDATE CASCADE
